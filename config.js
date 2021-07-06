500abce88bb768b42fe5b30bb3b2b42105e41597
yenvinguyen <87022278+yenvinguyen@users.noreply.github.com>
2021-07-06 21:18:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Thảo Ngân nòoooo!',
+    introDesc: `Khi kết thúc một ngày dài
+    Dù mọi chuyện có tốt hay xấu
+    Thì hãy luôn mỉm cười
+    Good luck`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải thi thật tốt đó😙',
+    desc: 'Thi tốt để Vi cho phép dẫn Vi đi ngắm mặt trời ☀️ ',
+    btnYes: 'okeee Vi con gà🐥 <33',
+    btnNo: 'Khồngggg 😠 :3',
+    question:'Mụt lời nhắn cho yenvinguyen <3',
+    btnReply: 'Gửi cho 🐣 <3',
+    reply: 'thi xong đi ☀️ <33333333',
+    mess: 'tui biết mà 🐣',
+    messDesc: 'Good luck 🍀',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100018570513313/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
