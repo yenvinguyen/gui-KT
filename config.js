7f01d84afd7f3bdf45d56e6b0e12bef023d9d5c9
yenvinguyen <87022278+yenvinguyen@users.noreply.github.com>
2021-07-20 22:17:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     introTitle: 'Thảo Ngân nòoooo!',
-    introDesc: `𝐀𝐢 𝐜𝐮̃𝐧𝐠 𝐩𝐡𝐚̉𝐢 𝐠𝐨́𝐢 𝐜𝐡𝐨 𝐦𝐢̀𝐧𝐡 𝐤𝐡𝐨𝐚̉𝐧𝐠 𝐭𝐫𝐨̛̀𝐢 𝐤𝐲́ 𝐮̛́𝐜
-    𝐀𝐢 𝐜𝐮̃𝐧𝐠 𝐩𝐡𝐚̉𝐢 𝐜𝐨́ 𝐭𝐫𝐨𝐧𝐠 𝐭𝐢𝐦 𝐦𝐨̣̂𝐭 𝐯𝐚̀𝐢 𝐯𝐞̂́𝐭 𝐭𝐡𝐮̛𝐨̛𝐧𝐠`,
+    introDesc: `Ai cũng phải gói cho mình khoảng trời kí ức
+    Ai cũng phải có một vài vết thương trong tim...`,
     btnIntro: 'to be countinue....',
     title: '𝐊𝐢̉ 𝐧𝐢𝐞̣̂𝐦 𝐜𝐡𝐢́𝐧𝐡 𝐥𝐚̀ 𝐭𝐡𝐮̛́ 𝐧𝐚̣̆𝐧𝐠 𝐧𝐞̂̀ 𝐧𝐡𝐚̂́𝐭, 𝐧𝐡𝐮̛𝐧𝐠 𝐫𝐨̂̀𝐢 𝐜𝐮̃𝐧𝐠 𝐜𝐡𝐢̉ 𝐥𝐚̀ 𝐜𝐡𝐮𝐲𝐞̣̂𝐧 đ𝐚̃ 𝐪𝐮𝐚 𝐦𝐚̀ 𝐭𝐡𝐨̂𝐢... nên là đừng nghĩ nhiều ',
     desc: 'khi kết thúc một ngày dài dù mọi chuyện thế nào thì hãy luôn mỉm cười nhaaaa!!! (thử không thì biết) ',
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'tui biết mà 🐣',
     messDesc: 'Phải thật vui nhá',
     btnAccept: 'Okiiiii <3',
-    messLink: 'https://www.facebook.com/messages/t/100018570513313/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink:  //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
