395087c8ae8571900e2760eecaa8e9d7b5a9463e
yenvinguyen <87022278+yenvinguyen@users.noreply.github.com>
2021-07-21 09:46:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: `Ai cũng phải gói cho mình khoảng trời kí ức
     Ai cũng phải có một vài vết thương trong tim...`,
     btnIntro: 'to be countinue....',
-    title: '𝐊𝐢̉ 𝐧𝐢𝐞̣̂𝐦 𝐜𝐡𝐢́𝐧𝐡 𝐥𝐚̀ 𝐭𝐡𝐮̛́ 𝐧𝐚̣̆𝐧𝐠 𝐧𝐞̂̀ 𝐧𝐡𝐚̂́𝐭, 𝐧𝐡𝐮̛𝐧𝐠 𝐫𝐨̂̀𝐢 𝐜𝐮̃𝐧𝐠 𝐜𝐡𝐢̉ 𝐥𝐚̀ 𝐜𝐡𝐮𝐲𝐞̣̂𝐧 đ𝐚̃ 𝐪𝐮𝐚 𝐦𝐚̀ 𝐭𝐡𝐨̂𝐢... nên là đừng nghĩ nhiều ',
+    title: 'Kỉ niệm chính là thứ nặng nề nhất, nhưng rồi cũng chỉ là chuyện đã qua mà thôi... nên là đừng nghĩ nhiều ',
     desc: 'khi kết thúc một ngày dài dù mọi chuyện thế nào thì hãy luôn mỉm cười nhaaaa!!! (thử không thì biết) ',
     btnYes: 'okeee 🐥 <33',
     btnNo: 'Khồngggg 😠 :3',
