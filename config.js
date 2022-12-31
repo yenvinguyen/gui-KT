49cffd5054b8ccd12394eed47c99b37ab099f029
yenvinguyen <87022278+yenvinguyen@users.noreply.github.com>
2022-12-31 23:55:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
 const CONFIG = {
-    introTitle: 'Thảo Ngân nòoooo!',
+    introTitle: 'Quỳnh Tứ nòoooo!',
     introDesc: `Ai cũng phải gói cho mình khoảng trời kí ức     
     Ai cũng phải có một vài vết thương trong tim...`,
     btnIntro: 'to be countinue....',
-    title: 'kỉ niệm chính là thứ nặng nề nhất, nhưng rồi cũng chỉ là chuyện đã qua mà thôi... nên là đừng nghĩ nhiều ',
-    desc: 'khi kết thúc một ngày dài dù mọi chuyện thế nào thì hãy luôn mỉm cười nhaaaa!!! (chị sẽ không từ chối được đâu ple ple) ',
+    title: 'một năm cũ đã qua và bây giờ là thời khắc bắt đầu chuyển sang năm mới. Vậy nên cậu đã có một năm rất tuyệt vời rồi và hãy bỏ lại tất cả ở sau. ',
+    title: 'Bắt đầu một năm mới thật tuyệt vời, gặt hái được nhiều thành công và mãi xinh nèeee! ',
+    desc: 'Hãy luôn tin vào bản thân và luôn mỉm cười nhaaaa!!! (bà sẽ không từ chối được đâu ple ple) ',
     btnYes: 'okeee 🐥 <33',
     btnNo: 'Khồngggg 😠 :3',
     question:'Mụt lời nhắn cho yenvinguyen <3',
-    btnReply: 'Gửi 🐣 <3',
+    btnReply: 'Gửi índi <3',
     reply: 'muốn gửi gắm gì thì ib trực típ nhó <33333333',
-    mess: '🐣 luôn luôn lắng nghe luôn luôn thấu hiểu',
+    mess: 'índi luôn luôn lắng nghe luôn luôn thấu hiểu',
     messDesc: 'Phải thật vui nhá',
     btnAccept: 'Okiiiii <3',
     messLink: 'https://www.facebook.com/messages/t/100011093524057/' //link mess của các bạn. VD: https://m.me/nam.nodemy
