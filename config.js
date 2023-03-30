40ca85260dbd948a2d5b1fa12fe214604d3fe936
yenvinguyen <87022278+yenvinguyen@users.noreply.github.com>
2023-03-30 22:29:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: `Dị là cũm đã cùng bên nhau gần nửa năm ùi. Người chị đã dạy em rất nhiều lun óoo. Và mỗi lần gặp nhau là một kỷ niệm khác nhau, hãy mãi ghi nhớ những kỷ niệm này nhóoooo`,
     btnIntro: 'to be countinue....',
     title: 'Cánh cửa này khép lại thì sẽ có cánh cửa khác mở ra nhòoo. Vậy nên chị đã có một năm rất tuyệt vời rồi và nhà BA mãi là nhà của chị ehee. ',
-    title: 'Chúc người chị luôn may mắn và thành công nheee <333 ! ',
+    title: 'Chúc người chị luôn may mắn nheee <333 ! ',
     desc: 'Gọi BA là nhà rùi mà hong chịu dìa lại là phạt 10 ly nheeee !!! (hong từ chối được đâu ple ple) ',
     btnYes: 'Chịu luôn nheee <33',
     btnNo: 'Khồngggg 😠 :3',
